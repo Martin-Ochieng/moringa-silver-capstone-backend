bc8c91ff1789679bb7176e4a506a785ebbdfc6c1
mochieng1 <martinochieng9360@gmail.com>
2025-04-30 19:44:21 +0300

    Updated to add validation: V2

--- src/main/java/com/example/demo/util/exceptionhandler/CustomException.java
+++ src/main/java/com/example/demo/util/exceptionhandler/CustomException.java
@@ -1,24 +1,24 @@
-package com.example.demo.util.exceptionhandler;
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import org.springframework.http.HttpStatus;
-
-@EqualsAndHashCode(callSuper = true)
-@AllArgsConstructor
-@Data
-public class CustomException extends RuntimeException  {
-    private final HttpStatus status;
-    private final String requestRefId;
-    private final String process;
-    private final transient Object object;
-    public CustomException(String message, HttpStatus status, String requestRefId, Object object, String process) {
-        super(message);
-        this.status = status;
-        this.requestRefId = requestRefId;
-        this.object = object;
-        this.process = process;
-    }
-
-}
+//package com.example.demo.util.exceptionhandler;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
+//import lombok.EqualsAndHashCode;
+//import org.springframework.http.HttpStatus;
+//
+//@EqualsAndHashCode(callSuper = true)
+//@AllArgsConstructor
+//@Data
+//public class CustomException extends RuntimeException  {
+//    private final HttpStatus status;
+//    private final String requestRefId;
+//    private final String process;
+//    private final transient Object object;
+//    public CustomException(String message, HttpStatus status, String requestRefId, Object object, String process) {
+//        super(message);
+//        this.status = status;
+//        this.requestRefId = requestRefId;
+//        this.object = object;
+//        this.process = process;
+//    }
+//
+//}
